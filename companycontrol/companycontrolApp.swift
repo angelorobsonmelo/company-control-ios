c3560e06ece4abc522b6ded712caa36a695d2848
angelomelo <angelorobsonmelo@gmail.com>
2023-05-10 14:11:22 -0300

    Initial Commit

---
+++ companycontrol/companycontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  companycontrolApp.swift
+//  companycontrol
+//
+//  Created by Ângelo Melo on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct companycontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
